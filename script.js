129586234a7216f693357daeb524a5ad69518e29
Maja Kulpa-Malecka <majaweronikakulpa@gmail.com>
2022-10-26 00:26:26 +1100

    make game work, basic styling

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let rows = 10;
-let columns = 8;
-let pictureArray = [];
 const table = document.getElementById("nonogram-table");
 
+let rows = 7;
+let columns = 8;
+let pictureArray = [];
+let winnerCells = [];
+
+/**
+ * Draw new game board
+ */
+
 function drawPicture() {
-  // includes 0 and 1, where 0 is an empty field
+  // includes random 0 and 1, where 0 is an empty cell
   for (let i = 0; i < columns * rows; i++) {
     pictureArray[i] = Math.round(Math.random());
   }
@@ -20,13 +26,14 @@ function loadNewBoard() {
   for (let i = 0; i < rows; i++) {
     let tableRow = document.createElement("tr");
     let tableHeaderCell = document.createElement("th");
-
-    tableHeaderCell.classList.add("v-header-cell");
-    table.appendChild(tableRow);
-    tableRow.appendChild(tableHeaderCell);
-    let horizontalGroups = [];
     let count = 0;
     let countSum = 0;
+
+    tableHeaderCell.classList.add("v-header-cell");
+    tableHeaderCell.classList.add("header");
+    table.appendChild(tableRow);
+    tableRow.appendChild(tableHeaderCell);
+    tableRow.classList.add("table-row");
 
     for (let k = i * columns; k < columns + i * columns; k++) {
       if (pictureArray[k] === 1) {
@@ -38,35 +45,44 @@ function loadNewBoard() {
         (pictureArray[k] === 1 && k + 1 === columns + i * columns)
       ) {
         countSum += count;
-        countSum > 0 && horizontalGroups.push(countSum);
+
+        const numberSpan = document.createElement("span");
+
+        if (countSum > 0) {
+          numberSpan.innerHTML = countSum;
+          tableHeaderCell.appendChild(numberSpan);
+        }
+
         count = 0;
         countSum = 0;
       }
-
-      tableHeaderCell.innerHTML = horizontalGroups.join(" ");
     }
 
     for (let j = 0; j < columns; j++) {
       let index = j + i * columns;
       let tableCell = document.createElement("td");
 
-      tableCell.setAttribute("tabindex", "1");
-      //   tableCell.style.background = pictureArray[index] && "#909090";
+      if (j === 0 && i === 0) {
+        tableCell.setAttribute("tabindex", "0");
+      } else {
+        tableCell.setAttribute("tabindex", "1");
+      }
+
       tableCell.id = `cell-${index}`;
       tableCell.classList.add("table-cell");
-      // pictureArray[index] && tableCell.classList.add("filled");
+      pictureArray[index] && winnerCells.push(tableCell);
       tableRow.appendChild(tableCell);
     }
   }
 
   for (let i = 0; i < columns; i++) {
     let tableHeaderCell = document.createElement("th");
-    tableHeaderCell.classList.add("h-header-cell");
-    tableHorizontalHeader.appendChild(tableHeaderCell);
-
-    let horizontalGroups = [];
     let count = 0;
     let countSum = 0;
+
+    tableHeaderCell.classList.add("h-header-cell");
+    tableHeaderCell.classList.add("header");
+    tableHorizontalHeader.appendChild(tableHeaderCell);
 
     for (let j = 0; j < rows; j++) {
       let index = i + j * columns;
@@ -80,55 +96,147 @@ function loadNewBoard() {
         (pictureArray[index] === 1 && index === columns * (rows - 1) + i)
       ) {
         countSum += count;
-        countSum > 0 && horizontalGroups.push(countSum);
+
+        const numberSpan = document.createElement("span");
+
+        if (countSum > 0) {
+          numberSpan.innerHTML = countSum;
+          tableHeaderCell.appendChild(numberSpan);
+        }
+
         count = 0;
         countSum = 0;
       }
-
-      tableHeaderCell.innerHTML = horizontalGroups.join(" ");
     }
   }
 }
 
-// Array.from(tableCells).forEach(function (element) {
-//   element.addEventListener("click", onCellClick);
-// });
+/**
+ * Cell click events
+ */
+
+function onCellClick(event) {
+  if (event.target.classList.contains("filled")) {
+    event.target.classList.remove("filled");
+  } else {
+    event.target.classList.add("filled");
+  }
+  if (event.target.classList.contains("crossed")) {
+    event.target.classList.remove("crossed");
+  }
+}
+
+function onCellDblClick(event) {
+  if (event.target.classList.contains("filled")) {
+    event.target.classList.remove("filled");
+  }
+  event.target.classList.add("crossed");
+}
+
+function onNumberClick(event) {
+  if (event.target.classList.contains("numberCrossed")) {
+    event.target.classList.remove("numberCrossed");
+  } else {
+    event.target.classList.add("numberCrossed");
+  }
+}
+
+function bindEvents() {
+  const tableCells = document.querySelectorAll(".table-cell");
+  const tableHeader = document.querySelectorAll(".header");
+
+  tableHeader.forEach((el) =>
+    el.addEventListener("click", (event) => {
+      onNumberClick(event);
+    })
+  );
+
+  tableCells.forEach((el) =>
+    el.addEventListener("click", (event) => {
+      onCellClick(event);
+    })
+  );
+
+  tableCells.forEach((el) =>
+    el.addEventListener("keyup", (event) => {
+      if (event.code === "Enter") {
+        onCellClick(event);
+      }
+      if (event.code === "Space") {
+        onCellDblClick(event);
+      }
+    })
+  );
+
+  tableCells.forEach((el) =>
+    el.addEventListener("dblclick", (event) => {
+      onCellDblClick(event);
+    })
+  );
+}
+
+/**
+ * Clear existing game
+ */
 
 function clearGame() {
   table.replaceChildren();
+  winnerCells = [];
+}
+
+/**
+ * Load new game
+ */
+
+function onLoad() {
+  drawPicture();
+  loadNewBoard();
+  bindEvents();
 }
 
 function loadNewGame() {
   clearGame();
-  drawPicture();
-  loadNewBoard();
+  onLoad();
 }
 
-drawPicture();
-loadNewBoard();
+onLoad();
 
-const tableCells = document.querySelectorAll(".table-cell");
+/**
+ * Check game solution
+ */
 
-const onCellClick = function () {
-  alert("click");
-};
+function checkSolution() {
+  const guessedCells = document.querySelectorAll(".filled");
+  const guessedArray = [...guessedCells];
+  const guesses = guessedArray.map((cell) => cell.id);
+  const winners = winnerCells.map((cell) => cell.id);
 
-tableCells.forEach((el) =>
-  el.addEventListener("click", (event) => {
-    if (event.target.classList.contains("filled")) {
-      event.target.classList.remove("filled");
-    } else {
-      event.target.classList.add("filled");
-    }
-    event.target.innerHTML = "";
-  })
-);
+  const gameWon =
+    guesses.length === winners.length &&
+    guesses.every((value, index) => value === winners[index]);
 
-tableCells.forEach((el) =>
-  el.addEventListener("dblclick", (event) => {
-    if (event.target.classList.contains("filled")) {
-      event.target.classList.remove("filled");
-    }
-    event.target.innerHTML = "X";
-  })
-);
+  gameWon
+    ? alert("Congratulations! You won!")
+    : alert("You lost... Keep trying!");
+}
+
+/**
+ * Give a clue on request, add fill or remove one if wrong
+ */
+
+function giveClue() {
+  const guessedCells = document.querySelectorAll(".filled");
+  const guessedArray = [...guessedCells];
+  const firstNotGuessed = winnerCells.find(
+    (cell) => !guessedArray.includes(cell)
+  );
+  const firstWrong =
+    guessedArray && guessedArray.find((cell) => !winnerCells.includes(cell));
+
+  firstNotGuessed?.classList.add("filled");
+  firstWrong?.classList.remove("filled");
+
+  if (!firstWrong && !firstNotGuessed) {
+    alert("Congratulations! You won!");
+  }
+}
